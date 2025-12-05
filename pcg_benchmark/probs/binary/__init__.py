c019544cea087cc3bcecbc4ccfec6b0a6d38b2bc
Jakub Kowalski <cutterthecat@gmail.com>
2025-12-05 17:18:00 +0100

    Added `binary-huge-v0`

--- pcg_benchmark/probs/binary/__init__.py
+++ pcg_benchmark/probs/binary/__init__.py
@@ -4,4 +4,5 @@ PROBLEMS = {
     "binary-v0": (BinaryProblem, {"width": 14, "height": 14}),
     "binary-wide-v0": (BinaryProblem, {"width": 28, "height": 14}),
     "binary-large-v0": (BinaryProblem, {"width": 28, "height": 28}),
+    "binary-huge-v0": (BinaryProblem, {"width": 60, "height": 60, "path": 120 }),
 }
