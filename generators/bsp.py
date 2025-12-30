766088f19bb17230e315985729e6744d78abe457
stanely1 <sb.reda456@gmail.com>
2025-12-30 19:58:02 +0100

    BSP generator improved

--- generators/bsp.py
+++ generators/bsp.py
@@ -4,16 +4,14 @@ import random
 
 
 class BspNode():
-    # TODO: parametrize this size
-    MIN_SIZE = 5
-
-    def __init__(self, parent, depth, x1, y1, x2, y2):
+    def __init__(self, parent, depth, x1, y1, x2, y2, min_room_size):
         self._parent = parent
         self._depth = depth
         self._x1 = x1
         self._y1 = y1
         self._x2 = x2
         self._y2 = y2
+        self._min_room_size = min_room_size
 
         self._rooms = []
 
@@ -22,21 +20,41 @@ class BspNode():
 
     def _check_size(self, dim):
         if dim == 'x':
-            return self._x2 - self._x1 >= 2 * BspNode.MIN_SIZE
+            return self._x2 - self._x1 >= 2 * (self._min_room_size + 2)
         elif dim == 'y':
-            return self._y2 - self._y1 >= 2 * BspNode.MIN_SIZE
+            return self._y2 - self._y1 >= 2 * (self._min_room_size + 2)
         else:
             return False
 
 
     def _init_room(self):
-        # TODO: parametrize room size
-        x1 = random.randint(self._x1 + 1, self._x2 - 3)
-        y1 = random.randint(self._y1 + 1, self._y2 - 3)
-        x2 = random.randint(x1 + 2, self._x2 - 1)
-        y2 = random.randint(y1 + 2, self._y2 - 1)
+        x1 = random.randint(self._x1 + 1, self._x2 - self._min_room_size)
+        y1 = random.randint(self._y1 + 1, self._y2 - self._min_room_size)
+        x2 = random.randint(x1 + self._min_room_size - 1, self._x2 - 1)
+        y2 = random.randint(y1 + self._min_room_size - 1, self._y2 - 1)
 
         self._rooms = [(x1, y1, x2, y2)]
+
+
+    def _dist(self, l, r, dim):
+        if dim == 'x':
+            intersection_start = max(l[1], r[1])
+            intersection_end = min(l[3], r[3])
+            if intersection_start <= intersection_end:
+                return r[0] - l[2]
+            elif r[3] < l[1]:
+                return r[0] - l[2] + l[1] - r[3]
+            else:
+                return r[0] - l[2] + r[1] - l[3]
+        else:
+            intersection_start = max(l[0], r[0])
+            intersection_end = min(l[2], r[2])
+            if intersection_start <= intersection_end:
+                return r[1] - l[3]
+            elif r[2] < l[0]:
+                return r[1] - l[3] + l[0] - r[2]
+            else:
+                return r[1] - l[3] + r[0] - l[2]
 
 
     def _split(self):
@@ -52,26 +70,33 @@ class BspNode():
             split_dim = random.choice('xy')
 
         if split_dim == 'x':
-            split_val = random.randint(self._x1 + BspNode.MIN_SIZE - 1, self._x2 - BspNode.MIN_SIZE)
-            self._left = BspNode(self, self._depth - 1, self._x1, self._y1, split_val, self._y2)
-            self._right = BspNode(self, self._depth - 1, split_val + 1, self._y1, self._x2, self._y2)
+            split_val = random.randint(self._x1 + self._min_room_size + 1, self._x2 - self._min_room_size - 2)
+            self._left = BspNode(self, self._depth - 1, self._x1, self._y1, split_val, self._y2, self._min_room_size)
+            self._right = BspNode(self, self._depth - 1, split_val + 1, self._y1, self._x2, self._y2, self._min_room_size)
         else:
-            split_val = random.randint(self._y1 + BspNode.MIN_SIZE - 1, self._y2 - BspNode.MIN_SIZE)
-            self._left = BspNode(self, self._depth - 1, self._x1, self._y1, self._x2, split_val)
-            self._right = BspNode(self, self._depth - 1, self._x1, split_val + 1, self._x2, self._y2)
+            split_val = random.randint(self._y1 + self._min_room_size + 1, self._y2 - self._min_room_size - 2)
+            self._left = BspNode(self, self._depth - 1, self._x1, self._y1, self._x2, split_val, self._min_room_size)
+            self._right = BspNode(self, self._depth - 1, self._x1, split_val + 1, self._x2, self._y2, self._min_room_size)
 
         self._rooms = self._left._rooms + self._right._rooms
 
         # connect siblings
+        min_dist = 10000000000000000000
+        closest_rooms = []
+        for room_left in self._left._rooms:
+            for room_right in self._right._rooms:
+                d = self._dist(room_left, room_right, split_dim)
+                if d < min_dist:
+                    min_dist = d
+                    closest_rooms = []
+                if d == min_dist:
+                    closest_rooms.append((room_left, room_right))
+
+        room_left, room_right = random.choice(closest_rooms)
+        min_x_left, min_y_left, max_x_left, max_y_left = room_left
+        min_x_right, min_y_right, max_x_right, max_y_right = room_right
+
         if split_dim == 'x':
-            idx_left, max_x_left = max(enumerate(self._left._rooms), key=lambda r: r[1][2])
-            max_x_left = max_x_left[2]
-            min_y_left, max_y_left = self._left._rooms[idx_left][1], self._left._rooms[idx_left][3]
-
-            idx_right, min_x_right = min(enumerate(self._right._rooms), key=lambda r: r[1][0])
-            min_x_right = min_x_right[0]
-            min_y_right, max_y_right = self._right._rooms[idx_right][1], self._right._rooms[idx_right][3]
-
             intersection_y_min = max(min_y_left, min_y_right)
             intersection_y_max = min(max_y_left, max_y_right)
 
@@ -85,14 +110,6 @@ class BspNode():
                 self._rooms.append((max_x_left, max_y_left, min_x_right, max_y_left))
                 self._rooms.append((min_x_right, max_y_left, min_x_right, min_y_right))
         else:
-            idx_left, max_y_left = max(enumerate(self._left._rooms), key=lambda r: r[1][3])
-            max_y_left = max_y_left[3]
-            min_x_left, max_x_left = self._left._rooms[idx_left][0], self._left._rooms[idx_left][2]
-
-            idx_right, min_y_right = min(enumerate(self._right._rooms), key=lambda r: r[1][1])
-            min_y_right = min_y_right[1]
-            min_x_right, max_x_right = self._right._rooms[idx_right][0], self._right._rooms[idx_right][2]
-
             intersection_x_min = max(min_x_left, min_x_right)
             intersection_x_max = min(max_x_left, max_x_right)
 
@@ -116,6 +133,7 @@ class Generator(search.Generator):
         self._height = self._env._problem._height
         self._target = self._env._problem._target
         self._depth = kwargs.get('depth', 4)
+        self._min_room_size = kwargs.get('min_room_size', 3)
 
         self._generate()
 
@@ -125,7 +143,7 @@ class Generator(search.Generator):
 
 
     def _generate(self):
-        root = BspNode(None, self._depth, 0, 0, self._width - 1, self._height - 1)
+        root = BspNode(None, self._depth, 0, 0, self._width - 1, self._height - 1, self._min_room_size)
 
         chromosome = search.Chromosome(self._random)
         chromosome.random(self._env)
